e3781bd6fc87127420519d84ae6373647e0ff3cf
Jake Wheeler <jakewheeler17@gmail.com>
2019-12-10 22:10:01 -0500

    temporarily remove plugin causing issue with build

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,13 +20,6 @@ module.exports = {
         path: `${__dirname}/src/images/`,
       },
     },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `posts`,
-        path: `${__dirname}/src/content/`,
-      },
-    },
     `gatsby-plugin-sass`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
