9037a3684c267197623a83f5b10f4778b99c1096
Jake Wheeler <jakewheeler17@gmail.com>
2019-12-10 22:05:15 -0500

    added content folder to hold markdown files to use instead of having content directly in the jsx on pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `posts`,
-        path: `${__dirname}/src/posts/`,
+        path: `${__dirname}/src/content/`,
       },
     },
     `gatsby-plugin-sass`,
