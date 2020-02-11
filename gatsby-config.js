0bf8a1fda191bc9184043f2006af6d716de49c00
Jake Wheeler <jakewheeler17@gmail.com>
2020-02-10 21:47:26 -0500

    adding analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,6 +49,12 @@ module.exports = {
         display: `minimal-ui`,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.ANALYTICS_ID,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
