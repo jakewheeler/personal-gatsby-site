cf8a07d09de473da5ad8864676ec5e3eeac00e14
Jake Wheeler <jakewheeler17@gmail.com>
2020-04-25 13:48:23 -0400

    made dark mode toggle look nicer

--- src/components/themeTogger.js
+++ src/components/themeTogger.js
@@ -1,18 +1,23 @@
 import React from "react"
 import { ThemeToggler } from "gatsby-plugin-dark-mode"
+import Toggle from "react-toggle"
+import { IoMdMoon, IoMdSunny } from "react-icons/io"
+import "../styles/toggler.css"
 
 export const DarkModeToggler = () => {
   return (
     <ThemeToggler>
       {({ theme, toggleTheme }) => (
-        <label>
-          <input
-            type="checkbox"
-            onChange={e => toggleTheme(e.target.checked ? "dark" : "light")}
-            checked={theme === "dark"}
-          />{" "}
-          Dark
-        </label>
+        <Toggle
+          className="custom-classname"
+          onChange={e => toggleTheme(e.target.checked ? "dark" : "light")}
+          checked={theme === "dark"}
+          aria-label="Dark mode"
+          icons={{
+            checked: <IoMdMoon />,
+            unchecked: <IoMdSunny />,
+          }}
+        />
       )}
     </ThemeToggler>
   )
