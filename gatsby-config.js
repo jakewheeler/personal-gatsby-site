186ac408efee75ffc53fa76e77d42c48ceed1d65
Jake Wheeler <jakewheeler17@gmail.com>
2020-08-13 21:36:46 -0400

    removed contentful deps

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,13 +5,6 @@ module.exports = {
     author: `@_jakewheeler`,
   },
   plugins: [
-    {
-      resolve: `gatsby-source-contentful`,
-      options: {
-        spaceId: process.env.CONTENTFUL_SPACE_ID,
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-      },
-    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
